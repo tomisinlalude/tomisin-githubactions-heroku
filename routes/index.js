2b6dcb661b0d477eba0750de0cc30e49cfdccc13
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-03-10 15:35:06 +0100

    Modify Dockerfile

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tomisin\'s Express App Built with Docker' });
 });
 
 module.exports = router;
